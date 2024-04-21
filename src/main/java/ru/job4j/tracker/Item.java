3d0cfa897b3fa0af50839a8d94c0bb655ea24d0f
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-04-21 20:56:55 +0300

    8. Date. Отображение даты. [#310017 #492365]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,18 +1,11 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-    private String currentDateTimeFormat = created.format(formatter);
-
-    public String getCurrentDateTimeFormat() {
-        return currentDateTimeFormat;
-    }
 
     public LocalDateTime getCreated() {
         return created;
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,9 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.format.DateTimeFormatter;
+
 public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-        System.out.println(item.getCurrentDateTimeFormat());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(item.getCreated().format(formatter));
     }
 }
