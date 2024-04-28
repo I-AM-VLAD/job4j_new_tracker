b3f3a74855b81cd58b298b72effe02eb3cef2b3e
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-04-28 17:10:04 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789 #10103]

--- src/main/java/ru/job4j/tracker/input/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker/input/ConsoleInput.java
@@ -2,20 +2,12 @@ package ru.job4j.tracker.input;
 
 import java.util.Scanner;
 
-import ru.job4j.tracker.input.Input;
-import ru.job4j.tracker.output.*;
-
 public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
-    private final Output output;
-
-    public ConsoleInput(Output output) {
-        this.output = output;
-    }
 
     @Override
     public String askStr(String question) {
-        output.println(question);
+        System.out.println(question);
         return scanner.nextLine();
     }
 
