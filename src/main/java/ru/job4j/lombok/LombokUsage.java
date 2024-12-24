c1e2f708a202dbf53ed16c4629d7ca4efb37e91e
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-12-24 21:05:05 +0200

    3. @Builder [#504890]

--- src/main/java/ru/job4j/lombok/LombokUsage.java
+++ src/main/java/ru/job4j/lombok/LombokUsage.java
@@ -2,8 +2,14 @@ package ru.job4j.lombok;
 
 public class LombokUsage {
     public static void main(String[] args) {
-        var bird = new BirdData();
-        bird.setAge(1);
-        System.out.println(bird);
+        var permission = Permission.of()
+                .id(1)
+                .name("name")
+                .rules("a")
+                .rules("b")
+                .rules("c")
+                .build();
+        System.out.println(permission);
+
     }
 }
